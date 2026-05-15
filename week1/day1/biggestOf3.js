6d6a5a7fbc877e18fa7aad1f2aa2d7b6a8d8910a
Dhairya Pandit <dhairyapandit45@gmail.com>
2026-05-15 14:17:29 +0530

    Update biggestOf3.js

--- week1/day1/biggestOf3.js
+++ week1/day1/biggestOf3.js
@@ -1,23 +1,14 @@
 // biggest of three
-let a= 20;
-let b= 40;
-let c= 10;
- if(a>b){
-    if(b>c){
-        console.log("a is bigger");
-    }
-    else 
-    {
-        console.log("b is bigger");
+let a = 20;
+let b = 40;
+let c = 10;
 
-    }
- }
- else if(b>c)
- {
+if (a > b && a > c) {
+    console.log("a is bigger");
+}
+else if (b > a && b > c) {
     console.log("b is bigger");
-    
- }
+}
 else {
     console.log("c is bigger");
 }
-
